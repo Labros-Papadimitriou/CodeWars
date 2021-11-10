ca698edec0c7f61738bfd9b827b8c7db187bdf68
Labros Papadimitriou <87263288+Labros-Papadimitriou@users.noreply.github.com>
2021-11-10 15:55:05 +0200

    added calculating-with-functions.py

---
+++ Python/calculating-with-functions.py
@@ -0,0 +1,59 @@
+def zero(default=0):
+    if default == 0:
+        return default
+    else:
+        return eval(f"0 {default}")
+def one(default=1):
+    if default == 1:
+        return default
+    else:
+        return eval(f"1 {default}")
+def two(default=2):
+    if default == 2:
+        return default
+    else:
+        return eval(f"2 {default}")
+def three(default=3):
+    if default == 3:
+        return default
+    else:
+        return eval(f"3 {default}")
+def four(default=4):
+    if default == 4:
+        return default
+    else:
+        return eval(f"4 {default}")
+def five(default=5):
+    if default == 5:
+        return default
+    else:
+        return eval(f"5 {default}")
+def six(default=6):
+    if default == 6:
+        return default
+    else:
+        return eval(f"6 {default}")
+def seven(default=7):
+    if default == 7:
+        return default
+    else:
+        return eval(f"7 {default}")
+def eight(default=8):
+    if default == 8:
+        return default
+    else:
+        return eval(f"8 {default}")
+def nine(default=9):
+    if default == 9:
+        return default
+    else:
+        return eval(f"9 {default}")
+
+def plus(number):
+    return f"+ {number}"
+def minus(number):
+    return f"- {number}"
+def times(number):
+    return f"* {number}"
+def divided_by(number):
+    return f"// {number}"
