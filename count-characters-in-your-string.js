fc181d939351c99fa6c22403a999252751401d16
Labros Papadimitriou <87263288+Labros-Papadimitriou@users.noreply.github.com>
2021-11-23 11:53:13 +0200

    Delete count-characters-in-your-string.js

--- count-characters-in-your-string.js
+++
@@ -1,8 +0,0 @@
-function count(str) {
-    let result = new Set(str);
-    obj = new Object;
-    for (let item of result) {
-        obj[item] = str.split(item).length - 1;
-    }
-    return obj;
-}
