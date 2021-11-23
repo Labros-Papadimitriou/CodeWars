23a7bc7efd4520c7e8990b9db336c56f8b232304
Labros Papadimitriou <87263288+Labros-Papadimitriou@users.noreply.github.com>
2021-11-23 11:53:30 +0200

    added count-characters-in-your-string.js

---
+++ JavaScript/count-characters-in-your-string.js
@@ -0,0 +1,9 @@
+
+function count(str) {
+    let result = new Set(str);
+    obj = new Object;
+    for (let item of result) {
+        obj[item] = str.split(item).length - 1;
+    }
+    return obj;
+}
