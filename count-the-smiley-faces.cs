ddb23e863e8b346ec389c11843f5add874fcc610
Labros Papadimitriou <87263288+Labros-Papadimitriou@users.noreply.github.com>
2022-01-04 20:35:05 +0200

    Delete count-the-smiley-faces.cs

--- count-the-smiley-faces.cs
+++
@@ -1,8 +0,0 @@
-using System.Linq;
-public static class Kata
-{
-  public static int CountSmileys(string[] smileys) 
-  {
-     return smileys.ToList().Count(c=>c==":)" || c==";)" || c==":-)" || c==":~)" || c==";-)" || c==";~)" || c==":D" || c==";D" || c==":-D" || c==":~D" || c==";-D" || c==";~D");
-  }
-}
