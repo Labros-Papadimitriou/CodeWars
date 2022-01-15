ded0b2ed84c09a50932453de72ac6c9052d00751
Labros Papadimitriou <87263288+Labros-Papadimitriou@users.noreply.github.com>
2022-01-15 19:44:52 +0200

    Update convert-pascalcase-string-into-snake_case.cs

--- C(sharp)/convert-pascalcase-string-into-snake_case.cs
+++ C(sharp)/convert-pascalcase-string-into-snake_case.cs
@@ -3,11 +3,6 @@ using System.Linq;
 
 public static class Kata 
 {
-  public static string ToUnderscore(int str) 
-  {
-    return str.ToString();
-  }
-
   public static string ToUnderscore(string str) 
   {
     return string.Concat(str.Select(x=>char.IsUpper(x)?"_"+x.ToString().ToLower():x.ToString())).TrimStart('_');
